ee8e1995b182f60b534e9e7862e518fe13fd7364
baisali609 <baisali609@gmail.com>
2025-02-18 08:51:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
-
+	cont boldElement = document.querySelectorAll("strong");
+	boldElement.forEach(element =>{
+		element.style.color ="rgb(0, 128, 0)";
+	});
 
 }
 
 
 function return_normal() {
     //Write your code here
+	cont boldElement = document.querySelectorAll("strong");
+	boldElement.forEach(element =>{
+		element.style.color ="rgb(0, 0, 0)";
+	});
 
     
 }
